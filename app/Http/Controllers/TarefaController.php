4b4c63290bec178e4d54244447eea46483ab66d9
MavihRodrigues <mavimariarodrigues@gmail.com>
2024-09-06 15:27:25 -0300

    first commit

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\TarefaService;
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    protected $tarefaService;
+
+    public function __construct(TarefaService $tarefaService)
+    {
+        $this->tarefaService = $tarefaService;
+    }
+    public function store (Request $request){
+        $tarefa = $this->tarefaService->create($request->all());
+        return response()->json($tarefa);
+    }
+}
